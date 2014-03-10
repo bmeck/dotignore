c2255b9fbc2a9266314b9c903ff45a1ea4215f79
Jordan Harband <ljharb@gmail.com>
2014-03-10 02:12:48 -0700

    Use an fake directory data structure rather than the filesystem, so many tests can be constructed without polluting the repo.
    ignore files and expected output are still in the filesystem.

--- test/test.js
+++ test/test.js
@@ -1,30 +1,44 @@
 var fs = require('fs');
 var path = require('path');
-var rules = String(fs.readFileSync(path.join(__dirname, '.ignore')));
+var rules = String(fs.readFileSync(path.join(__dirname, '.1\-ignore')));
 var matcher = require('../').createMatcher(rules);
 var test = require('tape');
 
+var checkDir = function checkDir(dir, paths, output) {
+  if (!output) { output = ''; }
+  paths.forEach(function (pathArr) {
+    var isDir = Array.isArray(pathArr);
+    var filename = isDir ? pathArr[0] : pathArr;
+    var resolved = path.join(dir, filename);
+    if (matcher.shouldIgnore(resolved)) {
+      output += ('- ' + resolved + '\n');
+    } else if (isDir) {
+      output = checkDir(resolved, pathArr[1], output);
+    } else {
+      output += ('+ ' + resolved + '\n');
+    }
+  });
+  return output;
+};
+
 test('expected output', function (t) {
   process.chdir(__dirname);
-  var output = '';
 
-  var checkDir = function checkDir(dir) {
-    fs.readdirSync(dir).forEach(function (filename) {
-      var resolved = path.join(dir, filename);
-      if (matcher.shouldIgnore(resolved)) {
-        output += ('- ' + resolved + '\n');
-      } else {
-        if (fs.statSync(resolved).isDirectory()) {
-          checkDir(resolved);
-        } else {
-          output += ('+ ' + resolved + '\n');
-        }
-      }
-    })
-  };
-  checkDir('.');
+  var root = [
+    '.ignore',
+    ['a', [
+      ['a', ['notignored']],
+      'ignored',
+      'notignored',
+      'notlisted'
+    ]],
+    'expected',
+    'test.js'
+  ];
 
-  t.equal(output, String(fs.readFileSync(path.join(__dirname, 'expected'))));
+  var output = checkDir('.', root);
+
+  t.equal(output, String(fs.readFileSync(path.join(__dirname, '1-expected'))));
   t.end();
 });
 
