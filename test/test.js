c048d965949bc9bb3b0b2363e9135a583127b841
Jordan Harband <ljharb@gmail.com>
2014-03-10 00:52:56 -0700

    Using tape in tests.

--- test/test.js
+++ test/test.js
@@ -2,26 +2,29 @@ var fs = require('fs');
 var path = require('path');
 var rules = fs.readFileSync(path.join(__dirname, '.ignore'))+'';
 var matcher = require('../').createMatcher(rules);
-var assert = require('assert');
-process.chdir(__dirname);
-var output = '';
-function checkDir(dir) {
-   fs.readdirSync(dir).forEach(function (filename) {
+var test = require('tape');
+
+test('expected output', function (t) {
+  process.chdir(__dirname);
+  var output = '';
+
+  var checkDir = function checkDir(dir) {
+    fs.readdirSync(dir).forEach(function (filename) {
       var resolved = path.join(dir, filename);
       if (matcher.shouldIgnore(resolved)) {
-         output += ('- '+resolved+'\n');
+        output += ('- ' + resolved + '\n');
+      } else {
+        if (fs.statSync(resolved).isDirectory()) {
+          checkDir(resolved);
+        } else {
+          output += ('+ ' + resolved + '\n');
+        }
       }
-      else {
-         if (fs.statSync(resolved).isDirectory()) {
-            checkDir(resolved);
-         }
-         else {
-            output += ('+ '+resolved+'\n');
-         }
-      }
-   })
-}
-checkDir('.');
+    })
+  };
+  checkDir('.');
 
-assert.equal(output, fs.readFileSync(path.join(__dirname, 'expected'))+'');
+  t.equal(output, fs.readFileSync(path.join(__dirname, 'expected')) + '');
+  t.end();
+});
 
