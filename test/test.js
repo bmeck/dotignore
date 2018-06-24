d31a3e3dd85f6be32506ce1774df8c5b43adfb93
Michael Ericksen <michael.ericksen@gmail.com>
2018-06-23 20:22:57 -0500

    fix comment lines that were treated as ignore specifications
    improve test coverage to assert expected results for windows and mac
    line separators. create additional test cases based on ignore file
    examples in Git's documentation.

--- test/test.js
+++ test/test.js
@@ -1,51 +1,148 @@
 var fs = require('fs');
 var path = require('path');
-var rules = String(fs.readFileSync(path.join(process.cwd(), 'test', '.1-ignore')));
-var matcher = require('../').createMatcher(rules);
 var test = require('tape');
+var runner = require('./testRunner');
+process.chdir(path.join(process.cwd(), 'test'));
 
-var checkDir = function checkDir(dir, paths, output) {
-  if (!output) { output = ''; }
-  paths.forEach(function (pathArr) {
-    var isDir = Array.isArray(pathArr);
-    var filename = isDir ? pathArr[0] : pathArr;
-    var resolved = path.join(dir, filename);
-    if (matcher.shouldIgnore(resolved)) {
-      output += ('- ' + resolved + '\n');
-    } else if (isDir) {
-      output = checkDir(resolved, pathArr[1], output);
-    } else {
-      output += ('+ ' + resolved + '\n');
-    }
-  });
-  return output;
-};
+var simpleRules = String(fs.readFileSync('.ignore'));
 
-test('expected output', function (t) {
-  process.chdir(path.join(process.cwd(), 'test'));
+test('returns expected results for mac file system', function (t) {
 
-  var root = [
+  var fileSystem = [
     '.ignore',
-    [
-      'a',
-      [
-        ['a', ['notignored']],
-        'ignored',
-        'notignored',
-        'notlisted'
-      ]
-    ],
+    'a/a/notignored',
+    'a/ignored',
+    'a/notignored',
+    'a/notlisted',
     'expected',
     'test.js'
   ];
 
-  var output = checkDir('.', root);
+  var expectedMacResults = JSON.parse(fs.readFileSync('test-expectations.json')).map(item => {
+    return {path: item.macPath, ignoreSymbol: item.ignoreSymbol};
+  });
+  
+  var matchedResults = runner.assertTestMatches(fileSystem, expectedMacResults, simpleRules);
 
-  t.equal(output, String(fs.readFileSync('1-expected')));
+  t.equal(fileSystem.length, matchedResults, 'all the file system files should be correctly marked')
   t.end();
 });
 
-test('delimiter defaults to path.sep', function (t) {
-  t.equal(matcher.delimiter, path.sep);
+
+test('returns expected results for windows file system', function (t) {
+
+  var fileSystem = [
+    '.ignore',
+    'a\\a\\notignored',
+    'a\\ignored',
+    'a\\notignored',
+    'a\\notlisted',
+    'expected',
+    'test.js'
+  ];
+
+  var expectedWinResults = JSON.parse(fs.readFileSync('test-expectations.json')).map(item => {
+    return {path: item.winPath, ignoreSymbol: item.ignoreSymbol};
+  });
+  
+  var matchedResults = runner.assertTestMatches(fileSystem, expectedWinResults, simpleRules);
+
+  t.equal(fileSystem.length, matchedResults, 'all the file system files should be correctly marked')
+  t.end();
+});
+
+test('should return + for .gitignore comment lines', function (t) {
+  var fileSystem = [
+    'package.json'
+  ];
+
+  var matchedResults = runner.assertTestMatches(fileSystem, [{path: 'package.json', ignoreSymbol: '+'}], simpleRules);
+
+  t.equal(fileSystem.length, matchedResults, 'the commented line with similar words should not cause package.json to be ignored');
+  t.end();
+});
+
+test('should correctly calculate ignore based on first git example', function(t) {
+  var fileSystem = [
+    'Documentation/foo.html',
+    'Documentation/gitignore.html',
+    'file.o',
+    'lib.a',
+    'src/internal.o'
+  ];
+
+  var expectedResults = [
+    {
+      "path": fileSystem[0],
+      "ignoreSymbol": "+"
+    },
+    {
+      "path": fileSystem[1],
+      "ignoreSymbol": "-"
+    },
+    {
+      "path": fileSystem[2],
+      "ignoreSymbol": "-",
+    },
+    {
+      "path": fileSystem[3],
+      "ignoreSymbol": "-"
+    },
+    {
+      "path": fileSystem[4],
+      "ignoreSymbol": "-"
+    }
+  ];
+
+  // Uses the first example from https://git-scm.com/docs/gitignore
+  // to assess the accuracy of the algorithm
+  var rules = String(fs.readFileSync('.gitignore-example-1'));
+  var matchedResults = runner.assertTestMatches(fileSystem, expectedResults, rules);
+
+  t.equal(fileSystem.length, matchedResults);
+  t.end();
+});
+
+// TODO: This test is currently failing. The issue seems to be that all
+// the lines in the gitignore file are rooted, i.e., '/', and the generated 
+// regular expression fails for any file path that includes one.
+test.skip('should correctly calculate ignore based on third git example', function(t) {
+  var fileSystem = [
+    'a/foo.html',
+    'a/b/foo.html',
+    'foo/baz.html',
+    'foo/raz/baz.html',
+    'foo/bar/baz.html'
+  ];
+
+  var expectedResults = [
+    {
+      path: fileSystem[0],
+      ignoreSymbol: '-'
+    },
+    {
+      path: fileSystem[1],
+      ignoreSymbol: '-'
+    },
+    {
+      path: fileSystem[2],
+      ignoreSymbol: '-'
+    },
+    {
+      path: fileSystem[3],
+      ignoreSymbol: '-'
+    },
+    {
+      path: fileSystem[4],
+      ignoreSymbol: '+'
+    }
+  ];
+
+  // Uses the third example from https://git-scm.com/docs/gitignore
+  // to assess the accuracy of the algorithm
+  var rules = String(fs.readFileSync('.gitignore-example-3'));
+  var matchedResults = runner.assertTestMatches(fileSystem, expectedResults, rules);
+
+  t.equal(fileSystem.length, matchedResults);
   t.end();
 });
